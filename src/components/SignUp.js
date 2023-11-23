41b9bbcc9b65625d0673e6350fb2014448514f4d
Tom Officer <thomasofficerjr@gmail.com>
2023-11-22 23:55:37 -0500

    added sign up modal styling

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { VStack, Input, Button, useToast } from '@chakra-ui/react';
+import { VStack, Input, Button, useToast, Box, Image } from '@chakra-ui/react';
 import { signUp } from '../services/authService.js';
+import logo from '../assets/img/logo.png';
 
 const SignUp = () => {
   //State variables
@@ -10,16 +11,7 @@ const SignUp = () => {
   //Define useToast variable (ChakraUI built-in error handling)
   const toast = useToast();
 
-  // const handleSignUp = async () => {
-  //   try {
-  //     await signUp(email, password);
-  //     // Handle success - maybe redirect to dashboard or show success message
-  //   } catch (error) {
-  //     console.error(error.message);
-  //     // Handle errors - show error messages to user
-  //   }
-  // };
-
+  //Signup handler with error handling (Chakra Toast)
   const handleSignUp = async () => {
     try {
       await signUp(email, password);
@@ -48,37 +40,82 @@ const SignUp = () => {
   return (
     <>
       <VStack>
+        <Box w='250px' mb='50px'>
+          <Image src={logo} />
+        </Box>
         <Input
+          bg='none'
+          border='2px solid #29f0cf'
+          borderRadius='68px'
+          w='300px'
+          h='50px'
           type='email'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder='Email'
+          color='white'
+          _focus={{ borderColor: '#05f2e6' }}
+          _hover={{
+            borderColor: '#05f2e6',
+          }}
         />
         <Input
+          bg='none'
+          border='2px solid #29f0cf'
+          borderRadius='68px'
+          w='300px'
+          h='50px'
           type='password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder='Password'
           mt='20px'
+          color='white'
+          _focus={{ borderColor: '#05f2e6' }}
+          _hover={{
+            borderColor: '#05f2e6',
+          }}
         />
-        <Button onClick={handleSignUp} colorScheme='blue' mt='50px'>
+        {/* <Button
+          onClick={handleSignUp}
+          bg='none'
+          border='2px solid #29f0cf'
+          borderRadius='68px'
+          px='40px'
+          py='30px'
+          color='#29f0cf'
+          _focus={{ borderColor: '#05f2e6' }}
+          _hover={{
+            borderColor: '#05f2e6',
+            color: '#05f2e6',
+            transform: 'scale(0.97)',
+          }}
+          mt='50px'>
           Sign Up
+        </Button> */}
+        <Button
+          bg='#29f0cf'
+          color='black'
+          fontFamily='Poppins'
+          borderRadius='68px'
+          px='40px'
+          py='25px'
+          mt='40px'
+          _hover={{ bg: '#05f2e6', transform: 'scale(0.97)' }}>
+          Sign Up
+        </Button>
+        <Button
+          color='#29f0cf'
+          bg='none'
+          fontFamily='Poppins'
+          px='40px'
+          py='25px'
+          mt='0px'
+          _hover={{ bg: 'none', transform: 'scale(0.97)' }}>
+          Sign In
         </Button>
       </VStack>
     </>
-    // <div>
-    //   <input
-    //     type='email'
-    //     value={email}
-    //     onChange={(e) => setEmail(e.target.value)}
-    //   />
-    //   <input
-    //     type='password'
-    //     value={password}
-    //     onChange={(e) => setPassword(e.target.value)}
-    //   />
-    //   <button onClick={handleSignUp}>Sign Down</button>
-    // </div>
   );
 };
 
--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -4,16 +4,28 @@ import {
   ModalContent,
   ModalBody,
   ModalCloseButton,
-  useToast,
 } from '@chakra-ui/react';
 import SignUp from './SignUp'; // Import your SignUp component
 
 const SignUpModal = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
-      <ModalOverlay />
-      <ModalContent py='50px'>
-        <ModalCloseButton />
+      <ModalOverlay sx={{ backdropFilter: 'blur(10px)' }} />
+      <ModalContent
+        maxW='900px'
+        w='500px'
+        h='auto'
+        pt='50px'
+        pb='40px'
+        bg='black'
+        // bg={{ backdropFilter: 'blur(5px)' }}
+        border='3px solid #29f0cf'
+        borderRadius='40px'>
+        <ModalCloseButton
+          m='10px'
+          color='#29f0cf'
+          _hover={{ transform: 'scale(1.2)' }}
+        />
         <ModalBody>
           <SignUp />
         </ModalBody>
