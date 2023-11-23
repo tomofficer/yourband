08ac768cb5a9ce52ab325e9ce205d7397b7fbf35
Tom Officer <thomasofficerjr@gmail.com>
2023-11-22 22:55:29 -0500

    added inputs to sign up modal, started styling

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { VStack, Input, Button } from '@chakra-ui/react';
 import { signUp } from '../services/authService.js';
 
 const SignUp = () => {
@@ -16,19 +17,39 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <input
-        type='email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type='password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignUp}>Sign Up</button>
-    </div>
+    <>
+      <VStack>
+        <Input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Email'
+        />
+        <Input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='Password'
+          mt='20px'
+        />
+        <Button onClick={handleSignUp} colorScheme='blue' mt='50px'>
+          Sign Up
+        </Button>
+      </VStack>
+    </>
+    // <div>
+    //   <input
+    //     type='email'
+    //     value={email}
+    //     onChange={(e) => setEmail(e.target.value)}
+    //   />
+    //   <input
+    //     type='password'
+    //     value={password}
+    //     onChange={(e) => setPassword(e.target.value)}
+    //   />
+    //   <button onClick={handleSignUp}>Sign Down</button>
+    // </div>
   );
 };
 
--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -9,24 +9,17 @@ import {
   ModalCloseButton,
   Button,
 } from '@chakra-ui/react';
-
 import SignUp from './SignUp'; // Import your SignUp component
 
 const SignUpModal = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Sign Up</ModalHeader>
+      <ModalContent py='50px'>
         <ModalCloseButton />
         <ModalBody>
           <SignUp />
         </ModalBody>
-        <ModalFooter>
-          <Button colorScheme='blue' mr={3} onClick={onClose}>
-            Close
-          </Button>
-        </ModalFooter>
       </ModalContent>
     </Modal>
   );
